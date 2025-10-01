64bf9cf69f5a2af3355d7278b589e1cb5102111d
maksympetliukh <maksympetliukh@icloud.com>
2025-10-01 19:13:17 +0200

    3

--- study.c
+++ study.c
@@ -1,9 +1,51 @@
+/*Tokens*/
+
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(){
+    //lexer - maximum munch
 
-    printf("Hello, World!\n");
+    //int my_var;
+    //int my_var0;
+    //int var = 0x11CA;
 
-    system("pause");
+    // ; end of operator
+
+    int a = -3;
+    char b = 7;
+    //a;
+    //a = b = a;
+    //a+;
+    //+a;
+    //a + b;
+    //a+ b;
+    //a +b;
+    //a++b; - error
+    //a ++b; - error
+    //a ++ b; - error
+    //a + + b;//a + (+b);
+    //printf(" b = %d\t", b);
+    //printf(" %zd \n", sizeof(+b));// type promotion
+    //a+++b; - a++ +b; - a +++b;
+    //a + ++b; - a + (++b);
+    //a + + +b; - a + (+(+b)) - a + + + b;
+    //a++++b; - error a++ ++b;
+    //a + +++b; - error a + +++b;
+    //a + + ++b; - a + (+(++b));
+    //printf("b = %d\t%zu\n",++b,  sizeof(+(++b)));
+    //a + + + + b; - a + (+(+(+b)));
+    //a++ + + b; - a++ + (+b); 
+    //a++++++b; - error
+    //a +++++b; (a++) ++ error
+    //Multiple consecutive '+' signs are considered a lexical error
+    //a + + ++ ++b; a + error
+    //a + + + + ++b; a + (+(+(+(++b))));
+    //a + + + + + +b; a + (+(+(+(+b))));
+    //a++++ + b; - error '++' needs l-value
+    //a+++ + + b; (a++) + (+(+b));
+    //a++ + + ++b; (a++) + (+(++b));
+
+    printf(" b = %d\tsize of b = %zu\n",b, sizeof(+ + b));
+    printf(" b = %d\tsize of b = %zu\n", ++b, sizeof(+ ++b));
+    return 0;
 }
