ca7a7f6036cfb3e901abc6b2dd5d136134cbacbb
maksympetliukh <maksympetliukh@icloud.com>
2025-10-01 19:42:49 +0200

    4

--- study.c
+++ study.c
@@ -1,51 +1,19 @@
-/*Tokens*/
+/*variables, identifiers and keywords*/
 
 #include <stdio.h>
 
 int main(){
-    //lexer - maximum munch
 
-    //int my_var;
-    //int my_var0;
-    //int var = 0x11CA;
+    int myVariable;
+    int my_variable;
+    int MyVariable;
 
-    // ; end of operator
+    //int 1023293892;//error 
+    //int 0Max; //error
+    //int my var;//error
+    //int my-var;//error
+    //int myvar, MyVar, myVar etc. - different variables!
+    //int _, int __, int ___, int _server__; unusable!!
 
-    int a = -3;
-    char b = 7;
-    //a;
-    //a = b = a;
-    //a+;
-    //+a;
-    //a + b;
-    //a+ b;
-    //a +b;
-    //a++b; - error
-    //a ++b; - error
-    //a ++ b; - error
-    //a + + b;//a + (+b);
-    //printf(" b = %d\t", b);
-    //printf(" %zd \n", sizeof(+b));// type promotion
-    //a+++b; - a++ +b; - a +++b;
-    //a + ++b; - a + (++b);
-    //a + + +b; - a + (+(+b)) - a + + + b;
-    //a++++b; - error a++ ++b;
-    //a + +++b; - error a + +++b;
-    //a + + ++b; - a + (+(++b));
-    //printf("b = %d\t%zu\n",++b,  sizeof(+(++b)));
-    //a + + + + b; - a + (+(+(+b)));
-    //a++ + + b; - a++ + (+b); 
-    //a++++++b; - error
-    //a +++++b; (a++) ++ error
-    //Multiple consecutive '+' signs are considered a lexical error
-    //a + + ++ ++b; a + error
-    //a + + + + ++b; a + (+(+(+(++b))));
-    //a + + + + + +b; a + (+(+(+(+b))));
-    //a++++ + b; - error '++' needs l-value
-    //a+++ + + b; (a++) + (+(+b));
-    //a++ + + ++b; (a++) + (+(++b));
-
-    printf(" b = %d\tsize of b = %zu\n",b, sizeof(+ + b));
-    printf(" b = %d\tsize of b = %zu\n", ++b, sizeof(+ ++b));
     return 0;
 }
