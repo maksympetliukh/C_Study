1fb27e3c19a028c8da5c980e7271f056697894c5
maksympetliukh <maksympetliukh@icloud.com>
2025-10-04 15:56:54 +0200

    8

--- study.c
+++ study.c
@@ -1,9 +1,18 @@
 /*Constants*/
 
 #include <stdio.h>
+#include <uchar.h>
+#include <wchar.h>
+
+#define GRAVITY 9.8f
+#define PI 3.141592
+#define LONG_DOUBLE 34.1234567890987654321L
 
 int main(){
 
+    printf("Gravity = %.1f\n",GRAVITY);
+
+#pragma region Literals Integer------------------------------------------
     int a = 10;
     printf("a = %d\n", a);
 
@@ -24,6 +33,66 @@ int main(){
 
     int d = b;
     printf("d = %d\n", d);
+
+    int e = 5;
+    //5 = e;//incorrect
+
+    short s_var = 5;
+    long i_var = 4;//equal to long int or int
+    long long int ll_var = 5LL;//5ll
+    unsigned int uint_var = 4U;//4u
+    unsigned long int ulint = 3UL;//3lu, 3ul
+
+    int x_var = 0x7A;
+    unsigned int u_var = 0xFEE;
+    unsigned long int ul_xvar = 0x2DUL;
+
+    int oct_var = 0127;
+    unsigned int u_oct_var = 0127U;
+    unsigned long int ul_oct_var = 0127UL;
+
+    int bi_var = 0b01010111;
+    unsigned int u_bi_var = 0b01010111U;
+    unsigned long int ul_bi_var = 0b01010111UL;
+    unsigned long long int ull_bi_var = 0b01010111ULL;
+
+#pragma endregion Literal Integers---------------------------------------
+
+#pragma region float------------------------------------------------------
+
+    float f_var0 = 3.14f;
+    float f_var1 = 3.f;
+    float f_var2 = .14f;
+
+    double f_var3 = 3.14;
+
+    float f_var4 = 3.14e+10f;
+    double f_var5 = 3.14e+10;
+    long double f_var6 = 3.14e+10L;
+
+    //float err1 = e+10f;
+    //float err2 = 3.14+10;
+    //float err3 = 3.14e;
+    //float err4 = 14ef;
+
+#pragma endregion float---------------------------------------------------
+
+    typedef char sbyte;
+#pragma region char-------------------------------------------------------
+
+    size_t st_var = 5;
+    printf("Size of st_var = %zd bytes\n", sizeof(st_var));
+
+    sbyte sb_var = 'A';
+    printf("Size of sb_var %zu byte\n", sizeof(sb_var));
+
+    sbyte var0 = 'B';
+    wchar_t var1 = 'B';
+    
+    char16_t var2 = 'C';
+    char32_t var3 = 'C';
+
+#pragma endregion char----------------------------------------------------
 
     return 0;
 }
