1c8810d12443bd9f71b553d408f527d20ce7e436
maksympetliukh <maksympetliukh@icloud.com>
2025-10-05 16:33:48 +0400

    12

---
+++ study.c
@@ -0,0 +1,14 @@
+/*Operators and Operands*/
+
+#include <stdio.h>
+
+int main() {
+#pragma region Variables Initialization and assignment operator------------------------------------
+	int a0;
+	long long a1 = 1;
+	long long a2 = 5;
+
+#pragma endregion
+
+	return 0;
+}
