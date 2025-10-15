a3501b1e26455f82e516c9b823cb0b3141582b6d
maksympetliukh <maksympetliukh@icloud.com>
2025-10-15 18:26:23 +0600

    loops while, do while, for

--- study.c
+++ study.c
@@ -1,52 +1,66 @@
-/*Updated binary operations exercises*/
-#include <stdio.h>
+/*Loops while, do while, for*/
 
-int main() {
-    int a = 5;
-    int b = 16;
-    int c = -3;
+ #include <stdio.h>
 
-    b >>= 1;
-    printf("B after binary division by 2 = %d\n", b);
+ int main() {
+     int i = 10;
+     while (i > 0) {--i; printf("i = %d\n", i);}
 
-    a ^= b;
-    b ^= a;
-    a ^= b;
-    printf("A after binary swapping = %d\nB after binary swapping = %d\n", a, b);
+     int c = -127;
+     while (++c){printf("c = %d\n", c);}
 
-    if (a & 1){printf("A is odd\n");}else{printf("A is even\n");}
+     char ch = -127;
+     while (--ch){printf("ch = %d\n", ch);}//overflowing
 
-    if (a & (1 << 3)){printf("Target bit of A is set\n");}else{printf("Target bit of A is clear\n");}
+     int x = 10;
+     while ((x--) > 0) {printf("x = %d\n", x);}
 
-    if (a != 0 && (a & (a - 1)) == 0) {printf("A is power of 2\n");}else{printf("A isn't power of 2\n");}
+     //while (1){printf("Infinite loop\n");}//Infinite loop
 
-    int lsb = b & -b;
-    int index = 0;
-    while ((lsb & 1) == 0){lsb >>= 1; index++;}
-    printf("LSB of B = %d\n", index);
+     int y = 10;
+     while (y <= 0 ? printf("y = %d\n", y) : --y){printf("While %d\n", y);}
 
-    b &= (b - 1);
-    printf("B after LSB clearing = %d\n", b);
+     //char buffer[100];
+     //char *pbuf = buffer;
+     //while ((*pbuf++ = getchar()) != '\n');
 
-    unsigned mask = a & (1u << 1) - 1;
-    unsigned result = a & mask;
-    printf("A after LSB-masking = %u\n", result);
+    // int count = 0;
+     //while (buffer[count] != '\n'){printf("%c",buffer[count - 1]);}
 
-    int sign = (c >>31) & 1;
-    if (sign == 0){printf("C sign is '+'\n");}else{printf("C sign is '-'\n");}
+     //pbuf = buffer - 1;
+     //while (*pbuf++ != '\n'){printf("%c", *pbuf);}
 
-    b |= (1 << 20);
-    b &= ~(1 << 7);
-    b ^= (1 << 0);
-    printf("B after standart binary operations = %d\n", b);
+     do{printf("hello\n");}while (0);
 
-    printf("Unsigned long int max value = %u\n", ~0UL);
-    printf("Int max value = %d\n", ~(1 << 31));
+     x = 5;
+     do{--x;printf("x = %d\n", x);}while (x > 0);
 
-    int x = a;
-    int count = 0;
-    while (x){x &= (x - 1);count++;}
-    printf("Count of set bits in A = %d\n", count);
+     x = 1;
+     do{x <<= 1;printf("x = %d\n", x);}while (x < 8);
 
-    return 0;
-}
+     //for (;;){}// infinite loop
+
+     for (int iter = 0; iter < 10; ++iter) {printf("iteration = %d\n", iter);}
+
+     for (int b = 10; b > 0; --b) {printf("b = %d\n", b);}
+
+     for (int i_1 = 0, i_2 = 0; i_1 < 5, i_2 < 3; i_1++, ++i_2){printf("I1 = %d\tI2 = %d\n", i_1, i_2);}
+
+     int e = 10;
+     for (;--e;){printf("e = %d\n", e);}//can be replaced with while loop
+
+     for (int var = 0;var <= 10; var++){if (var != 0 && (var % 2 == 0)) {printf("var = %d\n", var);}}
+
+     for (int var1 = 0; var1 < 1; var1++) {
+         printf("Loop enter\n");
+         for (int var2 = 0; var2 < 1; var2++) {
+             printf("loop works\n");
+         }
+         printf("Loop exit\n");
+     }
+
+     //int a = 0;
+     //for (int counter = -1; counter != 0; --counter) {a = counter; printf("a = %d\n", a);}//overflowing//undefined behavior//or 1
+
+   return 0;
+ }
