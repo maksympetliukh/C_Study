f7a788d7dc244e756cb2c2efe456317cb1dfe4d1
maksympetliukh <maksympetliukh@icloud.com>
2025-10-16 20:00:26 +0800

    Enum

--- study.c
+++ study.c
@@ -1,118 +1,30 @@
-/*Operators continue, break, go to*/
+/*Enumeration - enum*/
 #include <stdio.h>
 
+enum DAYS{Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday} yesterday, today = Sunday;
+enum CARS{BMW = -2, Audi, Opel = 5, VW, Nissan = 4, Toyota} car1 = Audi, car2 = VW, car3 = VW;
+enum COLOR{Red, Green, Blue} r = Red;
+
 int main() {
-    for (int i = 0; i < 10; ++i) {
+    yesterday = --today;
+    printf("Yesterday is %d day of week\n", yesterday);
 
-        if (i == 5){continue;}//skip i == 5 and continue loop
+    enum DAYS day;
+    day = Monday;
 
-        printf("Main loop %d\n", i);
+    day += 1;
+    printf("The second day of the week is day number %d\n", day);
+
+
+    enum CARS car4 = Toyota;
+
+    switch (r) {
+        case Red: printf("Red\n"); break;
+        case Green: printf("Green\n"); break;
+        case Blue: printf("Blue\n"); break;
+        default: printf("Default color\n");
     }
 
-    int a = 0;
-    while (a < 10) {
-
-        ++a;
-        if (a == 5){continue;}// skip  a == 5 and continue loop
-
-        printf("While loop %d\n", a);
-    }
-
-    for (int i = 0; i < 3; ++i) {
-        printf("Main loop %d\n", i);
-        for (int j = 0; j < 3; ++j) {
-            if (j == 2) {
-                continue;//skip j == 2 and continue nested loop
-            }
-            printf("Nested  loop %d\n", j);
-        }
-    }
-
-    for (int i = 0; i < 10; ++i) {
-        printf("Main loop %d\n", i);
-        if (i == 5) {
-            continue;
-        }
-        if (i == 8) {
-            break;//exit from nested loop before printf
-        }
-        printf("Nested loop %d\n", i);
-    }
-
-    int b = 5;
-    switch (b) {
-        case 1: printf("Switch %d\n", b);
-        case 2: printf("Switch %d\n", b);
-        case 3: printf("Switch %d\n", b);
-        case 4: printf("Switch %d\n", b);
-        case 5: printf("Switch %d\n", b);break;
-        default: printf("Switch default\n");
-    }
-
-    goto point1;
-
-    printf("Hello\n");//unusable while using goto
-
-    point1:printf("Point 1\n");
-
-    //goto point2;                   /
-    //point3: {printf("Hello\n");}  /useless
-    //point2:{printf("Point 2\n");}/
-    //goto point3;
-
-   // int f = 1;
-    //for (int i = 0; i < 10 && f; ++i) {
-        //printf("I-loop %d\n", i);
-        //for (int j = 0; j < 10; ++j) {
-            //printf("J-loop %d\n", j);
-            //for (int k = 0; k < 10; ++k) {
-                //printf("K-loop %d\n", k);
-                //if (k == 5) {
-                    //printf("Loop exit\n");
-                    //f = 0;
-                    //break;
-          //      }
-           // }
-       // }
-    //}
-
-    for (int i = 0; i < 10; ++i) {
-        printf("I-loop %d\n", i);
-        for (int j = 0; j < 10; ++j) {
-            printf("J-loop %d\n", j);
-            for (int k = 0; k < 10; ++k) {
-                printf("K-loop %d\n", k);
-                if (k == 5) {
-                    printf("End\n");
-                    goto label;
-                    break;
-                }
-            }
-        }
-    }
-    label:;//correct usage of goto in loops
-
-    int c = 3;
-    switch (c) {
-        case 0: printf("Switch %d\n", c);
-        p1: case 1: printf("Switch %d\n", c);break;
-        case 2: printf("Switch %d\n", c);
-        case 3: printf("Switch %d\n", c);              //correct usage of goto in switch
-        case 4:{printf("Switch %d\n", c); goto p1;}
-        case 5: printf("Switch %d\n", c);break;
-        default: printf("Switch default\n");
-    }
-
-    //int var = 5;
-    //if (var == 2){printf("var = %d\n", var); return 2;}
-    //if (var == 3){printf("var = %d\n", var); return 3;}
-   // if (var == 4){int temp = printf("%d\n", var); printf("temp = %d\n", temp); return temp;}
-    //if (var == 5){return ++var,  printf("var = %d\n", var);}
-
-    for (int i = 0; i < 10; ++i) {
-        printf(" i = %d\n", i);
-        if (i == 5){ return 1; }//return as loop exit
-    }
-
+    //In C23 standard we can set enum type// enum Count: char{One, Two, Three, ...};
     return 0;
 }
