4bcee7a35ec391be046e2934bbfa6f87961810ff
maksympetliukh <maksympetliukh@icloud.com>
2025-10-17 18:11:33 +0800

    addres of

--- study.c
+++ study.c
@@ -1,30 +1,20 @@
-/*Enumeration - enum*/
+/*Address of (&) */
+
 #include <stdio.h>
 
-enum DAYS{Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday} yesterday, today = Sunday;
-enum CARS{BMW = -2, Audi, Opel = 5, VW, Nissan = 4, Toyota} car1 = Audi, car2 = VW, car3 = VW;
-enum COLOR{Red, Green, Blue} r = Red;
-
 int main() {
-    yesterday = --today;
-    printf("Yesterday is %d day of week\n", yesterday);
+    int a = 5;
+    int *ptr_a = &a;
+    printf("Address of A: %p\n", &a);
+    printf("A value: %d\n", *ptr_a);
 
-    enum DAYS day;
-    day = Monday;
+    //printf("%p\n", &(a + a));//error, r-value
+    //printf("%p\n", &(a + 1));//error, r-value
 
-    day += 1;
-    printf("The second day of the week is day number %d\n", day);
+    //register int b = &a;//error, impossible to take address of register variable|
+    //printf("%p", &'A');/error
+    //printf("%p\n", &"Hello");//useless
+    //printf("%p\n", &28);//error
 
-
-    enum CARS car4 = Toyota;
-
-    switch (r) {
-        case Red: printf("Red\n"); break;
-        case Green: printf("Green\n"); break;
-        case Blue: printf("Blue\n"); break;
-        default: printf("Default color\n");
-    }
-
-    //In C23 standard we can set enum type// enum Count: char{One, Two, Three, ...};
     return 0;
 }
