30e21c0eb231c3ae13eeeb4e962b01425c2e3392
maksympetliukh <maksympetliukh@icloud.com>
2025-10-19 17:52:16 +0800

    1D array pointers

--- study.c
+++ study.c
@@ -1,41 +1,29 @@
-/* 2D and 3D arrays */
+/* 1D Array Pointers */
 
 #include <stdio.h>
 
 int main() {
-    int arr0[3][2] = {{1,2}, {3,4}, {5,6}};
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            printf("arr0[%d][%d] = %d\n", i, j, arr0[i][j]);
-        }
-    }
+    int arr[3] = {1, 2, 3};
+    int (*p0)[3] = &arr;
 
-    int arr1[2][2][2] = {{{0, 1}, {2, 3}}, {{4, 5}, {6, 7}}};
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            for (int k = 0; k < 2; k++) {
-                printf("arr1[%d][%d][%d] = %d\n", i, j, k, arr1[i][j][k]);
-            }
-        }
-    }
+    int x = 0;
+    //x = *p0;//incorrect//int cannot be int[]
+    //x = p0[1];//same
 
+    x = p0[0][1];//double dereference **p0[1];
+    printf("x = %d\n", x);
 
-    int x = **arr0;
-    printf("x=%d\n", x);
+    x = (*p0)[0];
+    printf("x = %d\n", x);
 
-    x = *(*(arr0 + 1) + 1);//arr0[1][1]
-    printf("x=%d\n", x);
+    x = **p0;//(*p0)[0];
+    printf("x = %d\n", x);
 
-    int y = ***arr1;
-    printf("y=%d\n", y);
+    x = **p0 + 2;//x = 3;(*p0)[2]//*(*p0 + 2)//x = po[0][2]//
+    printf("x = %d\n", x);
 
-    y = *(arr1[1][1] + 1);
-    printf("y=%d\n", y);
+    x = *(p0[0] + 1);//x = 2// x = *( *(p0[0] + 0) + 1);
+    printf("x = %d\n", x);
 
-    y = *(*(arr1[1] + 0) + 1);//arr1[1][0][1]
-    printf("y=%d\n", y);
-
-    y = *( *( *(arr1 + 0) +1) + 1);//arr1[0][1][1]
-    printf("y=%d\n", y);
     return 0;
 }
