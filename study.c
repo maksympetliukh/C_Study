1af0c6d4f4c4a628a71160abccac7b5cdd06e178
maksympetliukh <maksympetliukh@icloud.com>
2025-10-23 18:47:37 +0800

    Array of Characters

--- study.c
+++ study.c
@@ -1,48 +1,26 @@
-/* Pointers Array */
+/* Array of Characters */
 
 #include <stdio.h>
 
 int main() {
-    int arr[3] = {21, 8, 7};
+    char arr[5] = { 'A', 'B', 'C', 'D', 'E' };
 
-    int* parr[3] = {NULL};
+    printf("Arr 4 is %c\n", arr[4]);
+    //printf("Array's string %s\t", arr);//incorrect
 
-    parr[0] = &arr[0];
-    parr[1] = &arr[1];
-    parr[2] = &arr[2];
+    char str[6] = { 'A', 'B', 'C', 'D', 'E', '\0' };//use null character '\0' for correct string work
+    printf("String is %s\n", str);
 
-    printf("pArr Address is %p\n", parr);
-    printf("pArr 1 = %d\n", *parr[0]);
-    printf("pArr 2 = %d\n", *parr[1]);
-    printf("pArr 3 = %d\n", *parr[2]);
+    char str1[] = "hello";
+    printf("%s\n", str1);
 
-    char ch_arr[3] = {'C', 'B', 'A'};
-    char* ch_parr[3] = {NULL};
+    //char str1[50] = "hello"//incorrect, compiler is going to fill every empty element with zero
+    //char str1[2] = "hello"//incorrect
 
-    ch_parr[0] = &ch_arr[0];
-    ch_parr[1] = &ch_arr[1];
-    ch_parr[2] = &ch_arr[2];
+    char book[][3] = {"Hi", "Yo", "BB",{'a', 'b', '\0'}};
+    printf("%s\n", book[1]);
 
-    printf("ch_parr Address is %p\n", ch_arr);
-    printf("ch_parr 1 = %d\n", *ch_parr[0]);
-    printf("ch_parr 2 = %d\n", *ch_parr[1]);
-    printf("ch_parr 3 = %d\n", *ch_parr[2]);
-
-    printf("ch_parr 1 = %c\n", *ch_parr[0]);
-    printf("ch_parr 2 = %c\n", *ch_parr[1]);
-    printf("ch_parr 3 = %c\n", *ch_parr[2]);
-
-    int arr1[3] = {1, 2, 3};
-    int* arr1_ptr = arr1;
-
-    for (int i = 0; i < 3; i++) {arr1_ptr[i] = arr1[i]; printf("arr1[%d] = %d\n", i, arr1_ptr[i]);}
-
-    int x = 0;
-    printf("Address of x = %p\n", &x);
-
-    arr1_ptr[0] = x;
-    printf("arr1[0] = %d\n", arr1_ptr[0]);
-    printf("Address of arr1_ptr is %p\n", &arr1_ptr);
-
+    char format_arr[] = "Hello\nMy\n\"Kind\" World";
+    printf("%s\n", format_arr);
     return 0;
 }
