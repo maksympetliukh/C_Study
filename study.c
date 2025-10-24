f9d830c81b8388d0d13017e7ad3a19578b407b56
maksympetliukh <maksympetliukh@icloud.com>
2025-10-24 18:31:54 +0800

    Const

--- study.c
+++ study.c
@@ -1,21 +1,41 @@
-/* Pointers and Strings */
+/* Const and pointers */
 
 #include <stdio.h>
 
 int main() {
-    char str0[] = "Hello, my friend";
-    printf("%s\n", str0);
+    int v0 = 5;
+    printf("v0 = %d\n", v0);
 
-    char* p_str0 = str0;
-    printf("%s\n", p_str0);
-    printf("%c\n", p_str0[0]);
+    v0 = 20;
+    printf("new v0 = %d\n", v0);
 
-    //p_str0[0] = 'A';//incorrect, impossible to change the value of constant literal's element
+    const int v1 = 10;
+    printf("const v1 = %d\n", v1);
 
-    char str1[] = {'a', 'b', 'c', 'd', '\0'};//but now it's okay
-    char* p_str1 = str1;
-    p_str1[0] = 'F';
-    printf("%s\n", str1);
+    //v1 = 100;//error, const variable is read only!
+
+    int const v2 = 15;//const qualifier can be placed at any position
+    printf("const v2 = %d\n", v2);
+
+    int* p;
+
+    int* const p1 = &v0;//consists only one address!
+
+    const int* p2 = &v1;//read-only pointer! Consists only constant address
+
+    int const* p3 = &v2;//same
+
+    const int* const p4 = &v1;//cannot change both address and value
+
+    int const arr0[3];//array with 3 constant elements
+    const int arr1[3];//same
+
+    int const* arr2[4];// array with constant pointers
+    const int arr3[4];//same
+
+    int* const arr4[3];//array with constant elements, impossible to change address, but possible to change values at elements
+
+    const int* const arr5[3];//impossible to change address and value
 
     return 0;
 }
