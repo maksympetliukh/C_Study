ef302fdc7e3564f396d2813bed8903b9eda2c5df
maksympetliukh <maksympetliukh@icloud.com>
2025-11-02 19:27:13 +0700

    Arrays as function arguments

--- study.c
+++ study.c
@@ -1,56 +1,31 @@
-/* Arrays of Pointers to functions */
+/* Arrays as function arguments */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void func0(){printf("Function 0\n");}
-void func1(){printf("Function 1\n");}
-void func2(){printf("Function 2\n");}
+int arr[5] = {7, 15, 2, 78, 9};
 
-void(*p[])() = {func0,func1,func2};
+int func_arr1D(int arr[5]){printf("func_arr1D called\n");arr[3] = 777; return sizeof(arr);}//int func_arr1D(int* arr)
+int func_ptr(int* p_arr){printf("func_ptr called\n"); p_arr[3] = 777;*(p_arr + 1) = 25; return sizeof(p_arr);}
 
-int sum(int x, int y){printf("Sum = %d\n", x + y); return x + y;}
-int multiply(int x, int y){printf("Multiply = %d\n", x * y); return x * y;}
-
-double glob_data = 0;
-
-
-
-double* (*(*(*p2[5])(double* ))[3])(int*, int* );
-double* outer_func(int* a, int* b) {
-    double* pDouble = &glob_data;
-    *pDouble += *a + *b;
-    printf("Outer function = %f\n", glob_data);
-    return pDouble;
-}
-
-typedef  double* (*p_outer_func)(int* a, int* b);//double*(*)(int*, int*)
-
-p_outer_func arr[3] = {outer_func, outer_func, outer_func};
-
-p_outer_func* inner_func(double* a){p_outer_func arr1 = arr; return arr1;}
-
-typedef p_outer_func* (*p_inner_func)(double* a);//p_outer_func* (*)(double* a);
-p_inner_func arr2[3] = {inner_func, inner_func, inner_func};
-
-
+int func_arr2D(int arr2d[][3]){arr2d[0][3] = 333; return sizeof(arr2d);}
+int func_arr3D(int arr3d[][1][3]){arr3d[0][0][2] = 222; return sizeof(arr3d);}
 
 int main(int argc, char* argv[]) {
-    p[0]();
-    p[1]();
-    p[2]();
+    func_arr1D(arr);
+    func_ptr(arr);
 
-    for (int i = 0; i < 3; i++){p[i]();}
+    printf("Size of arr = %zd bytes\n", sizeof(arr));
+    printf("Size if arr in function = %d bytes\n", func_arr1D(arr));
+    printf("Size if arr in pointer function = %d bytes\n", func_ptr(arr));
 
-    int(*p1[])(int x, int y) = {sum,multiply};
-    for (int i = 0; i < 2; i++){p1[i](10, 4);}
+    int arr2D[2][3] = {{2, 3, 5}, {78, 11,35}};
+    printf("Size of arr2D = %zd bytes\n", sizeof(arr2D));
+    printf("Size of arr2D in function = %zd bytes\n", func_arr2D(arr2D));
 
-    int x = 5, y = 7;
-    (*arr[0])(&x, &y);
-    (*arr[1])(&x, &y);
-    (*arr[2])(&x, &y);
-
-    (*(*arr2[1])(&glob_data)[0])(&x, &y);
+        int arr3D[2][2][2] = {{{1, 2},{3, 4}}, {{5, 6}, {7, 8}}};
+    printf("Size of arr3D = %zd bytes\n", sizeof(arr3D));
+    printf("Size of arr3D in function = %zd bytes\n", func_arr3D(arr3D));
 
     return EXIT_SUCCESS;
 }
