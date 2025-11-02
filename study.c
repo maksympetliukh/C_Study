90e619fc8e70bc7480620983b31d7a4d2d81eb15
maksympetliukh <maksympetliukh@icloud.com>
2025-11-02 16:55:12 +0700

    Variative Functions

--- study.c
+++ study.c
@@ -1,43 +1,21 @@
-/* Preprocessor Directives Pt.3 */
+/*Variative Functions */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
-#define VERY_LONG_MACRO(a,b)\
-    printf("Very long message %d, %d\n", a, b)\
-    ;
-
-#define ALSO_LONG_MACRO(x, y) printf(#x " and " #y, x, y)//convert parameters to string literals
-
-#define JOIN_MACRO(a,b) printf("\n%d\n",a##b)
-
-#define GENERIC_JOINT(a, b, c) printf("%s\n",#a "" #b "" #c)
-
-#pragma once
-#include "exterfunc.h"
+int sum(int arg_count, ...) {
+    int temp = 0;
+    va_list arg_ptr;
+    va_start(arg_ptr, arg_count);
+    for (int i = 0; i < arg_count; i++) {temp += va_arg(arg_ptr, int);}
+    va_end(arg_ptr);
+    return temp;
+}
 
 int main(int argc, char* argv[]) {
-#pragma region Variables---
-    int a,b,c,d;
-#pragma endregion
-
-#pragma message("Message")
-
-#pragma pack(16)
-
-    struct Data {
-        char ch;
-        short x;
-        int y;
-    };
-
-    printf("Structure size is %lld bytes\n", sizeof(struct Data));
-
-
-    VERY_LONG_MACRO(2, 5);
-    ALSO_LONG_MACRO(3, 6);
-    JOIN_MACRO(3, 7);//37
-    GENERIC_JOINT(10, PRO, XL);
+    int s = sum(3, 4, 5, 6);
+    printf("%d\n", s);
 
     return EXIT_SUCCESS;
 }
