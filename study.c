9809bd39b17aa611e6f275abd5f5150e1bcb8ddd
maksympetliukh <maksympetliukh@icloid.com>
2025-11-08 16:05:03 +0100

    Dynamic memory allocation (Pt.1 Malloc)

--- study.c
+++ study.c
@@ -1,44 +1,56 @@
-/* Memory organization */
+/* Dynamic memory allocation (Pt.1 Malloc) */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "exterfunc.h"
 
-extern int extern_variable;//memory allocated in external file
+void Temp(int x, int y){char arr[] = {28, 9, 2};int z = x + y;}
 
-const int global_const = 0x1C;//data segment read-only
+int main(int argc, char *argv[]) {
+    int a = 5;
+    int b = 7;
+    //static memory allocation
 
-int global_int = 1;//data segment read-write
-int global_un_int;//BSS
-static int static_var = 0;//data segment read-write
-static int static_un_int;//BSS
+    Temp(a, b);//after Temp ends work, Temp will be deleted from stack, but garbage values of this function will stay saved on stack
+    int* a_ptr = &a;
+    int* b_ptr = &b;
 
-int global_arr[5] = { 1, 2, 3, 4 };//read-write
-char ch_arr[] = "Hello World";//read-write
+    int c = *a_ptr + *b_ptr;
 
-char* ch_ptr = "Hello World";//read-write
-int* ptr_global;//BSS
-int* ptr_global_init = NULL;//read-write
+    char ch_arr[5] = {'a', 'b', 'c', 'd', 'e'};//also static memory allocation
+    char* ch_ptr = (char*)malloc(1);
 
-int func0(int x) {                   //stack
-    static int count = 0;//read-write
-    static int static_un_init;//BSS
-    int y = 0;//stack
-    return x + y;//return on stack
-}
+    printf("c = %d\n", c);
 
-int main(int argc, char* argv[]) {
-    int local_var = 9;//stack
-    const int local_const = 21;//stack
-    int* ptr;//stack
-    int arr[5] = { 1, 2, 3, 4 };//stack
-    char ch_string[] = "Hello World";//stack
-    char* c_ptr = "Hello World";//stack
+    int* p_element = (int*)malloc(sizeof(int) * 5);//malloc allocates non-initialized memory with garbage value
+    printf("Address of the first memory location, allocated on heap for p_element is %p\n", &p_element);
 
-    int* ptr_heap;//stack
-    ptr_heap = malloc(sizeof(int) * 5);//heap
+    p_element[0] = 1;
+    p_element[1] = 2;
+    p_element[2] = 3;   //filling the memory locations, allocated in heap
+    p_element[3] = 4;
+    p_element[4] = 5;
 
-    func0(9);//stack
+    free(p_element);//makes current memory free fo OS but doesn't clear it
 
-    return  EXIT_SUCCESS;
+    short* p_short = (short*)malloc(sizeof(short) * 3);
+    printf("Address of the first memory location, allocated on heap for p_short is %p\n", &p_short);
+
+    int d = p_short[0];
+    printf("Garbage value at first memory location, allocated on heap for p_short %d\n", d);
+
+    free(p_short);
+
+    char* pString = (char*)malloc(sizeof(char) * 5 + 1);// +1 for '\0'
+    if (pString != NULL) {
+        pString[0] = 'H';
+        pString[1] = 'E';
+        pString[2] = 'L';
+        pString[3] = 'L';
+        pString[4] = 'O';
+
+        printf("%s\n", pString);
+    }
+    free(pString);
+
+    return EXIT_SUCCESS;
 }
