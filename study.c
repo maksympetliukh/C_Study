cf6b192943c0462743387a97afb52ffb6eaed2f8
maksympetliukh <maksympetliukh@icloid.com>
2025-11-08 17:03:07 +0100

    Calloc

--- study.c
+++ study.c
@@ -1,56 +1,34 @@
-/*Dynamic memory allocation - malloc*/
+/* Dynamic memory allocation - Calloc */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void Temp(int x, int y){char arr[] = {28, 9, 2};int z = x + y;}
-
 int main(int argc, char *argv[]) {
-    int a = 5;
-    int b = 7;
-    //static memory allocation
+    int* pVar0 = calloc(1, sizeof(int));//calloc allocates memory and initialize it with 0
+    printf("pVar0 = %d\n", *pVar0);//pVar0 = 0;
+    printf("Address of pVar0 = %p\n", pVar0);
+    free(pVar0);
 
-    Temp(a, b);//after Temp ends work, Temp will be deleted from stack, but garbage values of this function will stay saved on stack
-    int* a_ptr = &a;
-    int* b_ptr = &b;
+    char* pArr0 = calloc(5, sizeof(char));
+    if (pArr0 != NULL) {for (int i = 0; i < 5; i++) {printf("arr0[%d] = %d\n", i, pArr0[i]);}}
 
-    int c = *a_ptr + *b_ptr;
+    pArr0[0] = 'H';
+    pArr0[1] = 'e';
+    pArr0[2] = 'l';
+    pArr0[3] = 'l';
+    pArr0[4] = 'o';
 
-    char ch_arr[5] = {'a', 'b', 'c', 'd', 'e'};//also static memory allocation
-    char* ch_ptr = (char*)malloc(1);
+    if (pArr0 != NULL) {for (int i = 0; i < 5; i++) {printf("%c", pArr0[i]);}}
+    free(pArr0);
 
-    printf("c = %d\n", c);
+    int* input = calloc(1, sizeof(int));
+    if (input == NULL) {printf("\nIncorrect input\n"); return 1;}
 
-    int* p_element = (int*)malloc(sizeof(int) * 5);//malloc allocates non-initialized memory with garbage value
-    printf("Address of the first memory location, allocated on heap for p_element is %p\n", &p_element);
+    printf("\nPlease enter a number: ");
 
-    p_element[0] = 1;
-    p_element[1] = 2;
-    p_element[2] = 3;   //filling the memory locations, allocated in heap
-    p_element[3] = 4;
-    p_element[4] = 5;
-
-    free(p_element);//makes current memory free fo OS but doesn't clear it
-
-    short* p_short = (short*)malloc(sizeof(short) * 3);
-    printf("Address of the first memory location, allocated on heap for p_short is %p\n", &p_short);
-
-    int d = p_short[0];
-    printf("Garbage value at first memory location, allocated on heap for p_short %d\n", d);
-
-    free(p_short);
-
-    char* pString = (char*)malloc(sizeof(char) * 5 + 1);// +1 for '\0'
-    if (pString != NULL) {
-        pString[0] = 'H';
-        pString[1] = 'E';
-        pString[2] = 'L';
-        pString[3] = 'L';
-        pString[4] = 'O';
-
-        printf("%s\n", pString);
-    }
-    free(pString);
+    if (scanf("%d", input) == 1) {printf("Correct input\n");}else{printf("Incorrect input\n");}
+    free(input);
+    input = NULL;
 
     return EXIT_SUCCESS;
 }
