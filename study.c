ad90359836f5bcc0f7543c7d209647daeabc7d92
maksympetliukh <maksympetliukh@icloid.com>
2025-11-08 17:52:18 +0100

    Realloc

--- study.c
+++ study.c
@@ -1,34 +1,33 @@
-/* Dynamic memory allocation - Calloc */
+/* Dynamic memory allocation - Realloc */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    int* pVar0 = calloc(1, sizeof(int));//calloc allocates memory and initialize it with 0
-    printf("pVar0 = %d\n", *pVar0);//pVar0 = 0;
-    printf("Address of pVar0 = %p\n", pVar0);
-    free(pVar0);
+    char* pGreeting = (char*)malloc(6 * sizeof(char));
+    pGreeting[0] = 'H';
+    pGreeting[1] = 'e';
+    pGreeting[2] = 'l';
+    pGreeting[3] = 'l';
+    pGreeting[4] = 'o';
+    pGreeting[5] = '\0';
 
-    char* pArr0 = calloc(5, sizeof(char));
-    if (pArr0 != NULL) {for (int i = 0; i < 5; i++) {printf("arr0[%d] = %d\n", i, pArr0[i]);}}
+    if (pGreeting){{printf("%s\n", pGreeting);}}
 
-    pArr0[0] = 'H';
-    pArr0[1] = 'e';
-    pArr0[2] = 'l';
-    pArr0[3] = 'l';
-    pArr0[4] = 'o';
+    pGreeting= realloc(pGreeting, 12 * sizeof(char));
+    if (!pGreeting){pGreeting = NULL; return 1;}
 
-    if (pArr0 != NULL) {for (int i = 0; i < 5; i++) {printf("%c", pArr0[i]);}}
-    free(pArr0);
+    pGreeting[5] = ' ';
+    pGreeting[6] = 'W';
+    pGreeting[7] = 'o';
+    pGreeting[8] = 'r';
+    pGreeting[9] = 'l';
+    pGreeting[10] = 'd';
+    pGreeting[11] = '\0';
 
-    int* input = calloc(1, sizeof(int));
-    if (input == NULL) {printf("\nIncorrect input\n"); return 1;}
-
-    printf("\nPlease enter a number: ");
-
-    if (scanf("%d", input) == 1) {printf("Correct input\n");}else{printf("Incorrect input\n");}
-    free(input);
-    input = NULL;
+    if (pGreeting){printf("%s\n", pGreeting);}
+    free(pGreeting);
+    pGreeting = NULL;
 
     return EXIT_SUCCESS;
 }
