7c72fe155f252a3fa1161892292555fc1353d693
maksympetliukh <maksympetliukh@icloid.com>
2025-11-09 13:02:58 +0100

    _Generic

--- study.c
+++ study.c
@@ -1,110 +1,33 @@
-/* Dynamic arrays */
+/* _Generic */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ARRAY_SIZE 200000
-#define ROWS 2
-#define COLS 3
+#define GetType(T) _Generic((T), int: "Integer", double: "Double", default: "Unknown")
+
+void print_int(int x){printf("x = %d\n", x);}
+void print_double(double x){printf("x = %lf\n", x);}
+#define GET(x) _Generic((x), int: print_int, double: print_double, default: "Unknown")(x)
+
+void int_add(int x, int y){printf("int result = %d\n", x + y);}
+void double_add(double x, double y){printf("Double result = %lf\n", x + y);}
+void string_add(char* x, char* y){printf("String result = %s %s\n", x, y);}
+void char_add(char x, char y){printf("Char result = %c%c\n", x, y);}
+#define GENERIC_ADD(x,y) _Generic((x), int:int_add, double:double_add, char*: string_add, char: char_add, default: "Unknown")(x,y)
 
 int main(int argc, char *argv[]) {
-#pragma region Array_1D
-    int array[ARRAY_SIZE];//static memory allocation on stack
+    printf("%s\n",_Generic(25, int: "integer", char: "character", default: "unknown"));
 
-    int* p_array = malloc(200000 * sizeof(int));//dynamic allocation on heap
-    if (p_array != NULL) {
-        for (int i = 0; i < 200000; i++) {
-            p_array[i] = i;
-        }
-        printf("Allocation successful\n");
-    }else {
-        printf("Allocation failed\n");
-        exit(EXIT_FAILURE);
-    }
-    free(p_array);
-    p_array = NULL;
+    printf("%s\n", GetType(3));
+    printf("%s\n", GetType(3.1415));
 
-#pragma endregion
+    GET(5);
+    GET(5.128);
 
-#pragma region Array_2D
-
-    int (*ptr2D)[COLS] = (int(*)[COLS])malloc(sizeof(int) * ROWS * COLS);//24 bytes
-    if (ptr2D != NULL) {
-        printf("Size of 2D array is %lu bytes\n", sizeof(int) * ROWS * COLS);
-        ptr2D[0][2] = 50;
-        ptr2D[1][1] = 7;
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLS; j++) {
-                printf("%d ", ptr2D[i][j]);
-            }
-        }
-    }else {
-        printf("Allocation failed\n");
-        exit(EXIT_FAILURE);
-    }
-    free(ptr2D);
-    ptr2D = NULL;
-
-    int rows = 5;
-    int cols = 3;
-
-    int(*ptr2D_2)[cols] = calloc(rows * cols, sizeof(int));
-    if (ptr2D_2 != NULL) {
-        printf("\nSize of ptr2D_2 array is %lu bytes\n", sizeof(int) * rows * cols);
-        ptr2D_2[0][0] = 1;
-        ptr2D_2[0][1] = 2;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                printf("%d ", ptr2D_2[i][j]);
-            }
-        }
-    }else {
-        printf("Allocation failed\n");
-        exit(EXIT_FAILURE);
-    }
-    free(ptr2D_2);
-    ptr2D_2 = NULL;
-
-    int** ptr2D_3 = NULL;
-    int ptr_to_block = 2;
-    int block_0 = 4;
-    int block_1 = 6;
-
-    ptr2D_3 = (int**)malloc(sizeof(int**) * ptr_to_block);//2-steps allocation
-    if (ptr2D_3 != NULL) {
-        printf("\nAllocation successful\n");
-        printf("Size of ptr2D_3 is %lu bytes\n", sizeof(int**) * ptr_to_block);
-        ptr2D_3[0]= NULL;
-        ptr2D_3[0] = (int*)malloc(sizeof(int) * block_0);//allocation for the first block
-        if (ptr2D_3[0] != NULL) {
-            printf("Allocation for block_0 successful\n");
-        }else {
-            printf("Allocation for block_0 failed\n");
-            exit(EXIT_FAILURE);
-        }
-        ptr2D_3[1] = NULL;
-        ptr2D_3[1] = (int*)malloc(sizeof(int) * block_1);//allocation for the second block
-        if (ptr2D_3[1] != NULL) {
-            printf("Allocation for block_1 successful\n");
-        }else {
-            printf("Allocation for block_1 failed\n");
-            exit(EXIT_FAILURE);
-        }
-    }else {
-        printf("\nAllocation failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    free(ptr2D_3[0]);
-    ptr2D_3[0] = NULL;
-
-    free(ptr2D_3[1]);
-    ptr2D_3[1] = NULL;
-
-    free(ptr2D_3);
-    ptr2D_3 = NULL;
-
-#pragma endregion
+    GENERIC_ADD(5.1, 5.2);
+    GENERIC_ADD(7, 16);
+    GENERIC_ADD("Hello", "World");
+    GENERIC_ADD((char)'A', (char)66);
 
     return EXIT_SUCCESS;
 }
