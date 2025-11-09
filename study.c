9e0d5c7b7aafa5aa0eee32deb390314f57093d7a
maksympetliukh <maksympetliukh@icloid.com>
2025-11-09 12:15:43 +0100

    Dynamic arrays

--- study.c
+++ study.c
@@ -1,66 +1,110 @@
-/* Variable length arrays */
+/* Dynamic arrays */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-    //Function for example 2
-int vla_func(int arr_size, int vla[arr_size]) {
-    int mid = 0;
-    for (int i = 0; i < arr_size; i++) {
-        vla[i] = i;
-        printf("vla[%d] = %d\n", i, vla[i]);
-        mid += vla[i];
-    }
-    mid = mid / arr_size;
-    return mid;
-}
-
-    //Function for Example 3
-int sum(int rows, int cols, int arr[rows][cols]) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            arr[i][j] = i + j;
-        }
-    }
-    int temp = 0;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            temp += arr[i][j];
-        }
-    }
-    return temp;
-}
+#define ARRAY_SIZE 200000
+#define ROWS 2
+#define COLS 3
 
 int main(int argc, char *argv[]) {
-    #pragma region Example 1
-    int size0 = 0;
+#pragma region Array_1D
+    int array[ARRAY_SIZE];//static memory allocation on stack
 
-    printf("Enter size of array: ");
-    scanf("%d", &size0);
-
-    int array[size0];
-    for (int i = 0; i < size0; i++) {array[i] = i; printf("array[%d] = %d\n", i, array[i]);}
-#pragma endregion
-
-#pragma region Example 2
-
-    int size1 = 0;
-    printf("Enter size of array: ");
-    scanf("%d", &size1);
-
-    int array1[size1];
-    printf("Average value = %d", vla_func(size1, array1));
-#pragma endregion
-
-#pragma region Example 3
-
-    int x = 0, y = 0;
-    printf("Enter x, y: ");
-    scanf("%d%d", &x, &y);
-
-    int arr_2d[x][y];
-    printf("sum = %d", sum(size1, x, arr_2d));
+    int* p_array = malloc(200000 * sizeof(int));//dynamic allocation on heap
+    if (p_array != NULL) {
+        for (int i = 0; i < 200000; i++) {
+            p_array[i] = i;
+        }
+        printf("Allocation successful\n");
+    }else {
+        printf("Allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    free(p_array);
+    p_array = NULL;
 
 #pragma endregion
+
+#pragma region Array_2D
+
+    int (*ptr2D)[COLS] = (int(*)[COLS])malloc(sizeof(int) * ROWS * COLS);//24 bytes
+    if (ptr2D != NULL) {
+        printf("Size of 2D array is %lu bytes\n", sizeof(int) * ROWS * COLS);
+        ptr2D[0][2] = 45;
+        ptr2D[1][1] = 7;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                printf("%d ", ptr2D[i][j]);
+            }
+        }
+    }else {
+        printf("Allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    free(ptr2D);
+    ptr2D = NULL;
+
+    int rows = 5;
+    int cols = 3;
+
+    int(*ptr2D_2)[cols] = calloc(rows * cols, sizeof(int));
+    if (ptr2D_2 != NULL) {
+        printf("\nSize of ptr2D_2 array is %lu bytes\n", sizeof(int) * rows * cols);
+        ptr2D_2[0][0] = 1;
+        ptr2D_2[0][1] = 2;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                printf("%d ", ptr2D_2[i][j]);
+            }
+        }
+    }else {
+        printf("Allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    free(ptr2D_2);
+    ptr2D_2 = NULL;
+
+    int** ptr2D_3 = NULL;
+    int ptr_to_block = 2;
+    int block_0 = 4;
+    int block_1 = 6;
+
+    ptr2D_3 = (int**)malloc(sizeof(int**) * ptr_to_block);//2-steps allocation
+    if (ptr2D_3 != NULL) {
+        printf("\nAllocation successful\n");
+        printf("Size of ptr2D_3 is %lu bytes\n", sizeof(int**) * ptr_to_block);
+        ptr2D_3[0]= NULL;
+        ptr2D_3[0] = (int*)malloc(sizeof(int) * block_0);//allocation for the first block
+        if (ptr2D_3[0] != NULL) {
+            printf("Allocation for block_0 successful\n");
+        }else {
+            printf("Allocation for block_0 failed\n");
+            exit(EXIT_FAILURE);
+        }
+        ptr2D_3[1] = NULL;
+        ptr2D_3[1] = (int*)malloc(sizeof(int) * block_1);//allocation for the second block
+        if (ptr2D_3[1] != NULL) {
+            printf("Allocation for block_1 successful\n");
+        }else {
+            printf("Allocation for block_1 failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }else {
+        printf("\nAllocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    free(ptr2D_3[0]);
+    ptr2D_3[0] = NULL;
+
+    free(ptr2D_3[1]);
+    ptr2D_3[1] = NULL;
+
+    free(ptr2D_3);
+    ptr2D_3 = NULL;
+
+#pragma endregion
+
     return EXIT_SUCCESS;
 }
