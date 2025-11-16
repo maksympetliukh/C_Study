740566b0e2099cbb273efb05e6d834aa3ba65d2c
maksympetliukh <maksympetliukh@icloud.com>
2025-11-16 16:50:15 +0100

    Bit fields

--- study.c
+++ study.c
@@ -1,46 +1,111 @@
-/*Unions*/
+/*Bit fields*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-typedef union {
-    char ch;
-    int i;
+typedef struct {
+    char a : 4; //a.0000 0000 or 0000 0000.a
+}example;
+
+typedef struct {
+    unsigned char x : 4; //a.0000 b.0000
+    unsigned char y : 4;
+}new_example;
+
+typedef struct {
+    char a : 4; //(a.0000)0000 (b.000000)00
+    char b : 6;
+}BF0;
+
+typedef struct {
+    int var0 : 4; //(a.0000)0000 00000000 00000000 00000000
+}BF1;
+
+typedef struct {
+    int a : 4; //a.0000 (b.0000)0000000000000000000000000000
+    int b : 4; //or a.0000 (b.0000)0000 00000000 00000000 00000000
+}BF2;
+
+typedef struct {
+    int x : 4; //a.0000 (b.0000 00)000000 00000000 00000000
+    int y : 6; //or (a.0000)0000 (b.000000)00 000000000000000000000000
+}BF3;
+
+typedef struct {
+    int a : 17; //(a.00000000000000000) 000000000000000
+    int b : 17; //(b.00000000000000000) 000000000000000
+}BF4;
+
+typedef struct {
+    int a : 17;// (a.00000000 00000000 0)0000000 00000000
+    char b : 7;// (b.000000)0 000000000 00000000 00000000
+}BF5;
+
+#pragma pack(1)
+typedef struct {
+    int a : 17;//(a.00000000 00000000 0) (b.0000000 00000000) (c.0000000)0
+    int b : 15;
+    char c : 7;
+}BF6;
+
+typedef struct {
+    char a : 4;//(+-)a.0000//one bit for sign
+    unsigned char b : 4;//(+)b.0000
+}BF7;
+
+/*Error!!! Bit field can only be integer type
+typedef struct {
+    int a : 8;
+    double d : 5;
+}error;*/
+
+typedef struct {
+    u_char a : 8;
+    u_char b : 8;
+    struct{
+        u_char x : 8;
+        u_char y : 8;
+    }nested;
+}BF_PARENT;
+
+typedef struct {
+    uint32_t a : 4; //a.0000 b.0000 c.0000 d.0000 e.0000 f.0000 g.0000 h.0000
+    uint32_t b : 4; //Size of BF8 - 4 bytes
+    uint32_t c : 4;
+    uint32_t d : 4;
+    uint32_t e : 4;
+    uint32_t f : 4;
+    uint32_t g : 4;
+    uint32_t h : 4;
+}BF8;
+
+union BF_UNI {
+    int i : 4;
     double d;
-}mix;            //A union has size equal to his biggest field (element)
-
-mix m0;
-
-typedef union {
-    char ch;
-    int i;
-    double d;
-    struct s{int var_i; double var_d;} s;//union can store structures as member
-}super_mix;
-
-mix func(mix _mix){_mix.d = 111.222; return _mix;}//union as return value of function
+    //double d8 : 8; - error for gcc!!!
+};
 
 int main(void) {
-    m0.ch = 'A'; //A union cannot store several values of different members (elements) simultaneously
-    m0.i = 65;
-    m0.d = 3.1415;
+    example ex0 = {15};
+    example ex1 = {.a = 15};
+    example ex2 = {0b00001111};
+    example ex3 = {0b1111};
+    example ex4 = {0xF};
 
-    mix m1 = {0};//equal to m1.ch = 0;// initialization of the first element
+    new_example new_ex0 = {.x = 0x00000001, .y = 10};
 
-    mix* m2 = &m1;
-    m2->ch = 'B';
+    BF0 bf0 = {.a = 0b0001, .b = 0b00111111};
 
-    int* ptr = &m1.i;
-    *ptr = 500;
+    new_example* ptr_example = &new_ex0; //&new_ex0.x -error
+    ptr_example->x = 9;
+    ptr_example->y = 11;
+    printf("x = %d\ny = %d\n", ptr_example->x, ptr_example->y);
 
-    mix m_arr[5] = {{0}, [1].i = 97, [2].i = 100, [3].ch = 'C', [4].i = 1169};//initialization of every element by different members
-    for (int i = 0; i < 5; i++) {printf("m_arr[%d] = %d\n", i, m_arr[i].i);}
+    BF_PARENT bf_p0;
+    bf_p0.nested.x = 1;//access to nested structure field bf_p0.nested.x = 0b00000001
 
-    super_mix sm0 = {.s = {10, 20.544}};
-    printf("var_i = %d\nvar_d = %.3lf\n", sm0.s.var_i, sm0.s.var_d);
-    printf("Size of sm0 is %zu bytes\n", sizeof(sm0));//16 bytes - compiler aligned int field to 8 bytes
-
-    printf("_mix.d = %.3lf\n", func(m0).d);
+    printf("Size of BF8 is %zu bytes\n", sizeof(BF8));//4 bytes
 
     return EXIT_SUCCESS;
 }
