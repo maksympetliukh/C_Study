3b7a3fbe457f0b936dd968f53f9eb4ee94364f20
maksympetliukh <maksympetliukh@icloud.com>
2025-11-17 19:08:14 +0100

    GTK4.0 Pt.3

--- study.c
+++ study.c
@@ -1,24 +1,27 @@
-/*GTK 4.0 - */
+/*GTK 4.0*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
-static void app_activate(GApplication* app, gpointer* user_data) {
-    GtkWidget* window = gtk_application_window_new(GTK_APPLICATION(app));//create a window
-    gtk_window_set_title(GTK_WINDOW(window), "My Program");//set window title
-    gtk_window_set_default_size(GTK_WINDOW(window), 600, 600);//set window size
+static void app_activate (GApplication *app, gpointer* user_data) {
 
-    gtk_window_present(GTK_WINDOW(window));//show window
+    GtkWidget* main_window = gtk_application_window_new(GTK_APPLICATION(app));
+    gtk_window_set_title(GTK_WINDOW(main_window), "Create a button");
+    gtk_window_set_default_size(GTK_WINDOW(main_window), 500, 350);
+
+    GtkWidget* button = gtk_button_new_with_label("Click here");//create a button
+    gtk_window_set_child(GTK_WINDOW(main_window), button);//set a button as a window's element
+    gtk_window_present(GTK_WINDOW(main_window));
 }
 
 int main(int argc, char **argv) {
-    GtkApplication *app = gtk_application_new("my.prog", G_APPLICATION_DEFAULT_FLAGS);//initialization of application
-    g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);//set signal handler
+    GtkApplication* app = gtk_application_new("my.prog", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(app_activate), app);
 
-    int status = g_application_run(G_APPLICATION(app), argc, argv);//run application
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
 
-    g_object_unref(app);//free memory
+    g_object_unref(app);
 
     return status;
 }
