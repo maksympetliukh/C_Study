e6a702e81b2555ade844010cc96325636238dbc7
maksympetliukh <maksympetliukh@icloud.com>
2025-11-17 18:55:56 +0100

    GTK4.0 Pt.2

--- study.c
+++ study.c
@@ -1,24 +1,24 @@
-/*GTK 3.0 Library*/
+/*GTK 4.0 - */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
-#include <gdk/gdk.h>
-#include <gdk/gdkkeys.h>
-#include <gsk/gsk.h>
+
+static void app_activate(GApplication* app, gpointer* user_data) {
+    GtkWidget* window = gtk_application_window_new(GTK_APPLICATION(app));//create a window
+    gtk_window_set_title(GTK_WINDOW(window), "My Program");//set window title
+    gtk_window_set_default_size(GTK_WINDOW(window), 600, 600);//set window size
+
+    gtk_window_present(GTK_WINDOW(window));//show window
+}
 
 int main(int argc, char **argv) {
-    gtk_init();//initialization of widgets and toolset
+    GtkApplication *app = gtk_application_new("my.prog", G_APPLICATION_DEFAULT_FLAGS);//initialization of application
+    g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);//set signal handler
 
-    GtkWidget *window = gtk_window_new();//initialization of main window
+    int status = g_application_run(G_APPLICATION(app), argc, argv);//run application
 
-    gtk_window_set_title(GTK_WINDOW(window), "My first widget");//Set a main window header
-    gtk_window_set_default_size(GTK_WINDOW(window), 1000, 800);//Set window size
-    gtk_window_present(GTK_WINDOW(window));//show a window
+    g_object_unref(app);//free memory
 
-    while (g_list_model_get_n_items(gtk_window_get_toplevels()) > 0) {//start the loop of events listening and user interaction
-        g_main_context_iteration(NULL, true);//exit loop after every window will be closed
-    }
-
-    return EXIT_SUCCESS;
+    return status;
 }
