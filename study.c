7159277d6b3dbb2e988390495794cee2b269086b
maksympetliukh <maksympetliukh@icloud.com>
2025-11-18 19:07:20 +0100

    GTK 4.0 mixed sources

--- study.c
+++ study.c
@@ -1,26 +1,47 @@
-/*GTK 4.0*/
+/*GTK 4.0 mixed sources*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include <gdk/gdk.h>
 
-static void app_activate(GApplication* app, gpointer* user_data) {
-    //Create an object GtkBuilder and download interface definition from builder.ui
+static void app_activate(GtkApplication* app, gpointer* user_data) {
+    //define interface
+    const char* xml_label = "<interface><object class=\"GtkLabel\" id=\"label\"><property name=\"label\">Hello, my friend</property></object></interface>";
+
+    //create GtkBuilder object and upload interface definition from xml variable
     GtkBuilder* builder = gtk_builder_new_from_file("builder.ui");
 
-    //Get object GtkWindow by identifier "window"
+    //Upload interface definition from xml_label
+    gtk_builder_add_from_string(builder, xml_label, -1, NULL);
+
+    //Get object window by identifier "window"
     GObject* window = gtk_builder_get_object(builder, "window");
-    g_object_unref(builder);//Free GtkBuilder
-    gtk_window_set_application(GTK_WINDOW(window), app);//set application window
 
+    //Get object label by identifier "label"
+    GObject* label = gtk_builder_get_object(builder, "label");
+
+    //Add GtkLabel to GtkWindow
+    gtk_window_set_child(GTK_WINDOW(window), GTK_WIDGET(label));
+
+    //Free GtkBuilder
+    g_object_unref(builder);
+
+    //Set application window
+    gtk_window_set_application(GTK_WINDOW(window), app);
+
+    //Show application window
     gtk_window_present(GTK_WINDOW(window));
-}
 
+}
 int main(int argc, char** argv) {
-    GtkApplication* app = gtk_application_new("my.app", G_APPLICATION_DEFAULT_FLAGS);
+    //init application
+    GtkApplication* app = gtk_application_new("hello.world", G_APPLICATION_DEFAULT_FLAGS);
+    //connect with signal
     g_signal_connect(app, "activate", G_CALLBACK(app_activate), app);
+    //run application
     int status = g_application_run(G_APPLICATION(app), argc, argv);
+    //free memory
     g_object_unref(app);
 
     return status;
