28886ca0146f1cfa4e9554ccab0d6caad35fd7c5
maksympetliukh <maksympetliukh@icloud.com>
2025-11-19 17:54:51 +0100

    GTK4.0 - GtkBox

--- study.c
+++ study.c
@@ -1,42 +1,36 @@
-/*GTK 4.0 widget positioning*/
+/*GTK4.0 - GtkBox*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include <gdk/gdk.h>
 
-static void app_activate(GtkApplication* app, gpointer* user_data) {
-    GtkWidget* window = gtk_application_window_new(GTK_APPLICATION_WINDOW(app));
-    gtk_window_set_title(GTK_WINDOW(window), "Centred button");
-    gtk_window_set_default_size(GTK_WINDOW(window), 250, 200);
+static void app_activate(GApplication* app, gpointer* user_data) {
+    GtkWidget* window = gtk_application_window_new(GTK_APPLICATION(app));
+    gtk_window_set_title(GTK_WINDOW(window), "My GTK Box");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
 
-    GtkWidget* button = gtk_button_new_with_label("Hello");
+    //create container GtkBox
+    GtkWidget* box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+    gtk_widget_set_valign(box, GTK_ALIGN_START);
 
-    //horizontal aligning by left
-    gtk_widget_set_halign(button, GTK_ALIGN_START);
-    //vertical aligning
-    gtk_widget_set_valign(button, GTK_ALIGN_START);
+    //create a few buttons
+    GtkWidget* btn0 = gtk_button_new_with_label("Button 0");
+    GtkWidget* btn1 = gtk_button_new_with_label("Button 1");
+    GtkWidget* btn2 = gtk_button_new_with_label("Button 2");
 
-    //indent bottom
-    gtk_widget_set_margin_bottom(button, 10);
-    //indent right
-    gtk_widget_set_margin_end(button, 5);
-    //-//- left
-    gtk_widget_set_margin_start(button, 10);
-    //-//-top
-    gtk_widget_set_margin_top(button, 20);
+    //add all buttons to box
+    gtk_box_append(GTK_BOX(box), btn0);
+    gtk_box_append(GTK_BOX(box), btn1);
+    gtk_box_append(GTK_BOX(box), btn2);
 
-    gtk_window_set_child(GTK_WINDOW(window), button);
+    gtk_window_set_child(GTK_WINDOW(window), box);
     gtk_window_present(GTK_WINDOW(window));
 }
-int main(int argc, char** argv) {
-    //init application
-    GtkApplication* app = gtk_application_new("hello.world", G_APPLICATION_DEFAULT_FLAGS);
-    //connect with signal
-    g_signal_connect(app, "activate", G_CALLBACK(app_activate), app);
-    //run application
+int main(int argc, char **argv) {
+    GtkApplication* app = gtk_application_new("my.gtk.box", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
-    //free memory
     g_object_unref(app);
 
     return status;
